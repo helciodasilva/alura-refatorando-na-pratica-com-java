bc0f408a18dda4b2b7d976963a6b63881690743e
Helcio da Silva <helciodasilva@gmail.com>
2018-02-25 08:11:08 -0300

    Aula 3 - Exemplo 2

--- src/main/java/br/alura/refatoracao/cap3/TesteMatricula.java
+++ src/main/java/br/alura/refatoracao/cap3/TesteMatricula.java
@@ -8,7 +8,7 @@ public class TesteMatricula {
 	public static void main(String[] args) {
 		Matricula matricula = new Matricula(false, new GregorianCalendar(2020, Calendar.APRIL, 2));
 		
-		if (!matricula.isTrial() && matricula.getExpiracao().after(Calendar.getInstance())) {
+		if (matricula.estaValida()) {
 			System.out.println("Legal, vc eh um usuario com acesso ilimitado!!");
 		}
 		
