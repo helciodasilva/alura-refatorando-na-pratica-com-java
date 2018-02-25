162eb07a1c76ca24e923b2a4df73c6dee1949e28
Helcio da Silva <helciodasilva@gmail.com>
2018-02-25 08:10:37 -0300

    Aula 3 - Exemplo 1

---
+++ src/main/java/br/alura/refatoracao/cap3/TesteMatricula.java
@@ -0,0 +1,17 @@
+package br.alura.refatoracao.cap3;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TesteMatricula {
+
+	public static void main(String[] args) {
+		Matricula matricula = new Matricula(false, new GregorianCalendar(2020, Calendar.APRIL, 2));
+		
+		if (!matricula.isTrial() && matricula.getExpiracao().after(Calendar.getInstance())) {
+			System.out.println("Legal, vc eh um usuario com acesso ilimitado!!");
+		}
+		
+	}
+	
+}
